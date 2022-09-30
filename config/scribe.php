fdecdce2fb83c5d063a256951c92d0c69efb5e40
Sofyan Mahmoud <sofyan1020@gmail.com>
2022-10-01 01:57:55 +0200

    Fix(docs): Change the path of the output of the api documentation

--- config/scribe.php
+++ config/scribe.php
@@ -148,7 +148,7 @@ return [
          * HTML documentation, assets and Postman collection will be generated to this folder.
          * Source Markdown will still be in resources/docs.
          */
-        'output_path' => 'public/docs',
+        'output_path' => 'docs',
     ],
 
     /*
@@ -185,7 +185,7 @@ return [
          * Add a Try It Out button to your endpoints so consumers can test endpoints right from their browser.
          * Don't forget to enable CORS headers for your endpoints.
          */
-        'enabled' => true,
+        'enabled' => false,
 
         /**
          * The base URL for the API tester to use (for example, you can set this to your staging URL).
